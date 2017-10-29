fdacd71fb6e1c5b81fc9c04821b9e7f14e584c9f
GreyMag2141 <grey2141@yandex.ru>
2017-10-29 22:09:16 +1000

    2

---
+++ src/Zadacha105.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Zadacha105 {
+    public static void main(String[] args) {
+        System.out.println("Написать программу, которая находит корень линейного уравнения ax + b = 0");
+        Scanner in= new Scanner(System.in);
+        System.out.println("Введите a, b:");
+        int a = in.nextInt(), b = in.nextInt();
+        int x=-b/a;
+        System.out.println("x "+"= "+x);
+    }
+}
