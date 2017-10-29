a42cbf408f2a0bfda9b78822f92845e486cb3271
GreyMag2141 <grey2141@yandex.ru>
2017-10-29 21:36:13 +1000

    2

--- src/Zadacha101.java
+++ src/Zadacha101.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Zadacha101 {
     public static void main(String[] args) {
-    System.out.println("Ввести с клавиатуры натуральное число. Вывести на экран младший разряд числа.");
+    System.out.println("Ввести натуральное число. Вывести на экран младший разряд числа.");
     Scanner in= new Scanner(System.in);
     System.out.println("Введите натуральное число");
     int x=in.nextInt();
     int y=x%10;
-    System.out.println("Младший разряд числа"+"="+y);
+    System.out.println("Младший разряд числа ="+" " +y);
     }
 }
--- src/Zadacha102.java
+++ src/Zadacha102.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Zadacha102 {
     public static void main(String[] args) {
-        System.out.println("двузначное натуральное число.");
+        System.out.println("Ввести двузначное натуральное число. Вывести на экран количество десятков в нем.");
         Scanner in= new Scanner(System.in);
-        System.out.println("Введите натуральное число");
+        System.out.println("Введите двузначное натуральное число");
         int x=in.nextInt();
         int y=x/10;
-        System.out.println("количество десятков в нем"+"="+y);
+        System.out.println("Количество десятков "+"= "+y);
     }
 }
